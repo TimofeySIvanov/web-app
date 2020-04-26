4228a67ef617407469acc2757b07638aa20966d3
TimofeyIvanov <dasavto1@gmail.com>
2020-04-26 13:00:05 -0400

    alpha1.0

---
+++ my-app/src/components/courses/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const NotFound = () => (
+    <div className="main-content not-found">
+        <i className="material-icons inc-error">error_outline</i>
+        <h2>Page Not Found</h2>
+    </div>
+)
+export default NotFound
